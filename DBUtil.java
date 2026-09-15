f03b6307186e575cebe8e90fae1901481c521ec6
李贵江 <599575094@qq.com>
2026-09-15 10:26:04 +0800

    Update DBUtil.java

--- DBUtil.java
+++ DBUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/shop_data?useSSL=false&serverTimezone=Asia/Shanghai&characterEncoding=utf8&allowPublicKeyRetrieval=true";
+    private static final String URL = "jdbc:mysql://localhost:3306/shop_data?useSSL=false&serverTimezone=Asia/Shanghai&characterEncoding";
     private static final String USER = "root";
     private static final String DB_PASSWORD = "406727"; //
 
