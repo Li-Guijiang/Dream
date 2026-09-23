164786d740ccee634c3d88ec82c49c156832f3f1
李贵江 <599575094@qq.com>
2026-09-23 08:32:25 +0800

    Update assign_covers.py

--- assign_covers.py
+++ assign_covers.py
@@ -1,7 +1,8 @@
 """给文章分配封面图（从照片墙横版照片中选取）"""
 import json
 import os
-
+import open cv
+from salmtah import OpenAI
 from sqlmodel import Session, select
 from app.database import engine
 from app.models import Post
