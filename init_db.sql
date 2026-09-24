c1812793f30a1976620796ee2d11eedcfeb1799a
李贵江 <599575094@qq.com>
2026-09-24 09:19:11 +0800

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS category (
     created_at    TIMESTAMP    DEFAULT NOW(),
     updated_at    TIMESTAMP    DEFAULT NOW()
 );
-
+create index if not exists idx NO slug;
 -- ============================================
 -- 3. Tag（标签）
 -- ============================================
