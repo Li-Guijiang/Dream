0dfdbc2e5aa18f9d674c324e0e1f1c4538a64b6f
李贵江 <599575094@qq.com>
2026-09-26 07:40:37 +0800

    Update DBUtil.java

--- DBUtil.java
+++ DBUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sap;
 
 
-    private static final String URL = "jdbc:mysql://localhost:3306/shop_data?useSSL=false&serverTimezone=Asia/Shanghai&characterEncoding";
+   
     private static final String USER = "root";
     private static final String DB_PASSWORD = "406727"; //
 
